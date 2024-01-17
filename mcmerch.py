27007885708642269a6421ccd4a56cd19ad05696
Nehad <nehad.st@gmail.com>
2024-01-17 00:18:54 -0500

    selling unitary goods in Ontario (close <#1>)

---
+++ mcmerch.py
@@ -0,0 +1,24 @@
+import sys # this allows us to figure out what the first argument is
+
+def parseArguments():
+    arguments = {
+        "price" : int(sys.argv[1]),
+        "quantity" : 1,
+        "province" : "ON"
+    }
+    return arguments
+
+def taxRate (province):
+    tax = {
+        "ON" : 0.13,
+    }
+    return tax[province]
+
+def mcmerchCalculator():
+    arguments = parseArguments()
+    tax = taxRate(arguments['province'])
+    result = arguments['price'] * arguments['quantity'] * (1 + tax)
+    rounded_result = round(result, 2)  # Round the result to 2 decimal places
+    print(rounded_result)
+
+mcmerchCalculator()
